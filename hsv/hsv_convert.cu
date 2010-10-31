923386ded4d5b10c97c0395b78ee0c3726d6f5f8
Patrick Rogers <helloresolven@gmail.com>
2010-10-30 22:21:29 -0400

    you cannot have external __device__ functions... unless you have a fermi card. oh and histogram produces image.

--- hsv/hsv_convert.cu
+++ hsv/hsv_convert.cu
@@ -1,6 +1,89 @@
 #include <cuda.h>
 #include <cuda_runtime_api.h>
-#include "hsv_convert.cuh"
+
+__device__ uchar4 convert_one_pixel_to_rgb(float4 pixel) {
+	float r, g, b;
+	float h, s, v;
+	
+	h = pixel.x;
+	s = pixel.y;
+	v = pixel.z;
+	
+	float f = h/60.0f;
+	float hi = floorf(f);
+	f = f - hi;
+	float p = v * (1 - s);
+	float q = v * (1 - s * f);
+	float t = v * (1 - s * (1 - f));
+	
+	if(hi == 0.0f || hi == 6.0f) {
+		r = v;
+		g = t;
+		b = p;
+	} else if(hi == 1.0f) {
+		r = q;
+		g = v;
+		b = p;
+	} else if(hi == 2.0f) {
+		r = p;
+		g = v;
+		b = t;
+	} else if(hi == 3.0f) {
+		r = p;
+		g = q;
+		b = v;
+	} else if(hi == 4.0f) {
+		r = t;
+		g = p;
+		b = v;
+	} else {
+		r = v;
+		g = p;
+		b = q;
+	}
+	
+	unsigned char red = (unsigned char) __float2uint_rn(255.0f * r);
+	unsigned char green = (unsigned char) __float2uint_rn(255.0f * g);
+	unsigned char blue = (unsigned char) __float2uint_rn(255.0f * b);
+	unsigned char alpha = (unsigned char) __float2uint_rn(pixel.w);
+	return (uchar4) {red, green, blue, alpha};
+}
+
+__device__ float4 convert_one_pixel_to_hsv(uchar4 pixel) {
+	float r, g, b, a;
+	float h, s, v;
+	
+	r = pixel.x / 255.0f;
+	g = pixel.y / 255.0f;
+	b = pixel.z / 255.0f;
+	a = pixel.w;
+	
+	float max = fmax(r, fmax(g, b));
+	float min = fmin(r, fmin(g, b));
+	float diff = max - min;
+	
+	v = max;
+	
+	if(v == 0.0f) { // black
+		h = s = 0.0f;
+	} else {
+		s = diff / v;
+		if(diff < 0.001f) { // grey
+			h = 0.0f;
+		} else { // color
+			if(max == r) {
+				h = 60.0f * (g - b)/diff;
+				if(h < 0.0f) { h += 360.0f; }
+			} else if(max == g) {
+				h = 60.0f * (2 + (b - r)/diff);
+			} else {
+				h = 60.0f * (4 + (r - g)/diff);
+			}
+		}		
+	}
+	
+	return (float4) {h, s, v, a};
+}
 
 __global__ void convert_to_hsv(uchar4 *rgb, float4 *hsv, int width, int height) {
 	int x = threadIdx.x + blockIdx.x * blockDim.x;
@@ -36,4 +119,54 @@ extern "C" void convert_to_rgb_wrapper(float4 *hsv, uchar4 *rgb, int width, int 
 	dim3 blocks((width + 15)/16, (height + 15)/16);
 	
 	convert_to_rgb<<<blocks, threads>>>(hsv, rgb, width, height);
+}
+
+__global__ void compute_histogram(float4 *hsv, unsigned int *histogram, int width, int height) {
+	__shared__ unsigned int cached_histogram[360];
+	
+	int x = threadIdx.x + blockIdx.x * blockDim.x;
+	int y = threadIdx.y + blockIdx.y * blockDim.y;
+	
+	cached_histogram[threadIdx.x] = 0;
+	
+	if(x < width && y < height) {
+		float h = hsv[x + y*width].x;
+		int hue = max(0, min(359, (int) h));
+		atomicAdd(&(cached_histogram[hue]), 1);
+	}
+	__syncthreads();
+	
+	atomicAdd(&(histogram[threadIdx.x]), cached_histogram[threadIdx.x]);
+}
+
+__global__ void draw_image(unsigned int *histogram, uchar4 *image) {
+	__shared__ unsigned int max;
+
+	int x = threadIdx.x;
+	
+	unsigned int count = histogram[x];
+	if(x == 0) { max = count;}
+	
+	atomicMax(&max, count);
+	__syncthreads();
+	unsigned int scaled_size = (250 * count) / max;
+	uchar4 rgb_pixel = convert_one_pixel_to_rgb((float4) {(float) x, 1.0f, 1.0f, 255.0f});
+	uchar4 black = (uchar4) {0,0,0,255};
+	
+	uchar4 *paintbrush = &rgb_pixel;
+	
+	int y;
+	for(y = 0; y < 256; y++) {
+		image[x + (255-y)*360] = *paintbrush;
+		if(y > scaled_size) { paintbrush = &black; }
+	}
+}
+
+extern "C" void compute_histogram_image(float4 *hsv, unsigned int *histogram, uchar4 *image, int width, int height) {
+	dim3 threads(360,1);
+	dim3 blocks((width + 359)/360, height);
+	
+	compute_histogram<<<blocks, threads>>>(hsv, histogram, width, height);
+	
+	draw_image<<<1, threads>>>(histogram, image);
 }
--- hsv/main.c
+++ hsv/main.c
@@ -10,14 +10,16 @@
 
 extern void convert_to_hsv_wrapper(uchar4 *rgb, float4 *hsv, int width, int height);
 extern void convert_to_rgb_wrapper(float4 *hsv, uchar4 *rgb, int width, int height);
-
+extern void compute_histogram_image(float4 *hsv, unsigned int *histogram, uchar4 *image, int width, int height);
 int main (int argc, char* argv[]) {
-	uchar4 *host_in, *host_out, *host_golden;
+	uchar4 *host_in, *host_out;
 	
 	uchar4 *rgb_pixels;
 	float4 *hsv_pixels;
 	
-	int *host_histogram;
+	unsigned int *histogram;
+	unsigned int *host_histogram;
+	
 	png_t *info;
 	
 	if(argc < 2) {
@@ -32,35 +34,36 @@ int main (int argc, char* argv[]) {
 		
 	size_t number_of_bytes_rgb = sizeof(uchar4) * info->width * info->height;
 	size_t number_of_bytes_hsv = sizeof(float4) * info->width * info->height;
+	size_t number_of_bytes_hist_img = sizeof(uchar4) * 360 * 256;
+	size_t number_of_bytes_histogram = sizeof(unsigned int) * 360;
 	
-	host_out = malloc(number_of_bytes_rgb);
-	host_golden = malloc(number_of_bytes_rgb);
+	host_out = malloc(number_of_bytes_hist_img);
+	host_histogram = malloc(number_of_bytes_histogram);
 	
 	cudaMalloc((void **) &rgb_pixels, number_of_bytes_rgb);
-	assert(cudaGetLastError() == cudaSuccess);
-	cudaMalloc((void **) &hsv_pixels, number_of_bytes_hsv);	
-	assert(cudaGetLastError() == cudaSuccess);
+	cudaMalloc((void **) &hsv_pixels, number_of_bytes_hsv);
+	cudaMalloc((void **) &histogram, number_of_bytes_histogram);
+	cudaMemset(histogram, 0, number_of_bytes_histogram);
+		
 	cudaMemcpy(rgb_pixels, host_in, number_of_bytes_rgb, cudaMemcpyHostToDevice);
 	assert(cudaGetLastError() == cudaSuccess);
 	
 	convert_to_hsv_wrapper(rgb_pixels, hsv_pixels, info->width, info->height);
 	
-	convert_to_rgb_wrapper(hsv_pixels, rgb_pixels, info->width, info->height);
+	cudaFree(rgb_pixels);
+	cudaMalloc((void **) &rgb_pixels, number_of_bytes_hist_img);
 	
-	cudaMemcpy(host_out, rgb_pixels, number_of_bytes_rgb, cudaMemcpyDeviceToHost);
-	assert(cudaGetLastError() == cudaSuccess);
+	compute_histogram_image(hsv_pixels, histogram, rgb_pixels, info->width, info->height);
+	cudaMemcpy(host_out, rgb_pixels, number_of_bytes_hist_img, cudaMemcpyDeviceToHost);
 	
 	char *output_file;	
 	if(argc > 3) { output_file = argv[2]; } 
 	else { output_file = "data/out.png"; }
-	
+	info->width = 360;
+	info->height = 256;
 	if(write_png(output_file, info, host_out) == PNG_FAILURE) {
-		printf("Error writing to file (%s)\n", "data/out.png");
+		printf("Error writing to file (%s)\n", output_file);
 	}
-	
-	free(host_golden);
-	free(host_in);
-	free(host_out);
 
 	return 0;
 }
